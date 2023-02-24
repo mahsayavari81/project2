890ac5e511dbab71bacae91c0830a38dd5ff86d9
mhsayavri <mahsahd81@gmail.com>
2023-02-24 23:05:34 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace project1
+{
+    class test
+    {
+        public static void Main()
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            int num1 = Convert.ToInt32(input1);
+            int num2 = Convert.ToInt32(input2);
+
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
